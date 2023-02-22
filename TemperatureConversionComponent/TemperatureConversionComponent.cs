27aef1a7ef2261d89cf71f5b5bb97cca3902ce60
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2023-02-22 17:32:53 +0300

    Add project files.

---
+++ TemperatureConversionComponent/TemperatureConversionComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+namespace TempConv
+{
+    public class TemperatureConversionComponent
+    {
+        public TemperatureConversionComponent()
+        {
+        }
+        public double cToF(double c)
+        {
+            // how to control output format
+            return (int)((c * 9 / 5.0 + 32) * 100) / 100.0;
+        }
+        public double fToC(double f)
+        {
+            // how to control output format
+            return (int)((f - 32) * 5 / 9.0 * 100) / 100.0;
+        }
+    }
+}
